738a2bf6eac586cfbe82a20ca404c7e2b269a19d
Josue <josue@localhost.localdomain>
2015-12-11 10:31:50 -0600

    test

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,8 @@
  (c) 2014-2015 Stormpath, Inc. http://stormpath.com
  License: Apache 2.0
 */
+/*
+ Stormpath ID Site v0.2.3
+ (c) 2014-2015 Stormpath, Inc. http://stormpath.com
+ License: Apache 2.0
+*/
